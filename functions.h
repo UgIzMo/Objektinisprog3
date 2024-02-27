728c5356a08165d844c69ad9626da6dda01f92a9
UgIzMo <152986822+UgIzMo@users.noreply.github.com>
2024-02-27 17:35:30 +0200

    Update functions.h

--- functions.h
+++ functions.h
@@ -3,22 +3,19 @@
 
 #include <vector>
 #include <string>
-
 #include "studentas.h"
 
-using namespace std;
-
-double skaiciuotiVidurki(const vector<int>& nd);
-double skaiciuotiMediana(const vector<int>& namuDarbai);
-double skaiciuotiGalutini(const vector<int>& namuDarbai, int egzaminas, bool naudotiVidurki);
+double skaiciuotiVidurki(const std::vector<int>& nd);
+double skaiciuotiMediana(const std::vector<int>& namuDarbai);
+double skaiciuotiGalutini(const std::vector<int>& namuDarbai, int egzaminas, bool naudotiVidurki);
 void atsitiktiniai(Studentas& studentas);
 void atsitiktiniaiStudentai(Studentas& studentas);
-void nuskaitymas(vector<Studentas>& studentai, const string& failoPavadinimas);
+void nuskaitymas(std::vector<Studentas>& studentai, const std::string& failoPavadinimas);
 bool rusiuotiPagalVarda(const Studentas& a, const Studentas& b);
 bool rusiuotiPagalPavarde(const Studentas& a, const Studentas& b);
 bool rusiuotiPagalGalutiniVidurki(const Studentas& a, const Studentas& b);
 bool rusiuotiPagalGalutiniMediana(const Studentas& a, const Studentas& b);
-void spausdinimas(const vector<Studentas>& studentai, const string& isvedimoFailas = "");
-string pasirinktiFaila();
+void spausdinimas(const std::vector<Studentas>& studentai, const std::string& isvedimoFailas = "");
+std::string pasirinktiFaila();
 
-#endif
+#endif // FUNCTIONS_H
