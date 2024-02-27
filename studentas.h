514853fc6bd9d9c47eaaad1574607d49e5e0a19b
UgIzMo <152986822+UgIzMo@users.noreply.github.com>
2024-02-27 17:37:38 +0200

    Add files via upload

---
+++ studentas.h
@@ -0,0 +1,15 @@
+#ifndef STUDENTAS_H
+#define STUDENTAS_H
+
+#include <string>
+#include <vector>
+
+struct Studentas
+{
+    std::string vardas;
+    std::string pavarde;
+    std::vector<int> namuDarbai;
+    int egzaminas;
+};
+
+#endif // STUDENTAS_H
