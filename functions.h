18993feb2b211c080684682c9bc02886c7a9230f
UgIzMo <152986822+UgIzMo@users.noreply.github.com>
2024-03-04 23:11:01 +0200

    Update functions.h

--- functions.h
+++ functions.h
@@ -5,14 +5,10 @@
 #include <string>
 #include "studentas.h"
 
-void generateFiles();
+void generateStudentFiles(const std::vector<int>& sizes);
 void skaitymasGeneravimo(std::vector<Studentas> &studentai, const std::string &failoPavadinimas);
 void surusiuotiIrIsvesti(const std::vector<Studentas> &studentai, const std::string &vargsiukaiFailas, const std::string &kietiakiaiFailas);
-double skaiciuotiVidurki(const std::vector<int>& nd);
-double skaiciuotiMediana(const std::vector<int>& namuDarbai);
-double skaiciuotiGalutini(const std::vector<int>& namuDarbai, int egzaminas, bool naudotiVidurki);
-void atsitiktiniai(Studentas& studentas);
-void atsitiktiniaiStudentai(Studentas& studentas);
+
 void nuskaitymas(std::vector<Studentas>& studentai, const std::string& failoPavadinimas);
 bool rusiuotiPagalVarda(const Studentas& a, const Studentas& b);
 bool rusiuotiPagalPavarde(const Studentas& a, const Studentas& b);
