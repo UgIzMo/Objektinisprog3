60ac7a5311d26b30d6b00323477cb75bf8876054
UgIzMo <152986822+UgIzMo@users.noreply.github.com>
2024-03-19 14:00:03 +0200

    Update functions_vector.h

--- functions_vector.h
+++ functions_vector.h
@@ -6,6 +6,8 @@
 
 void readDataVector(std::vector<Studentas>& studentai, const std::string& failoVardas);
 void generateStudentFilesVector(int size);
-void rusiuotStudentusVector(const std::string& failoVardas);
 
-#endif // FUNKCIJOSVECTOR_H
+void rusiuotStudentusVector1(const std::string& failoVardas);
+void rusiuotStudentusVector2(const std::string& failoVardas);
+void rusiuotStudentusVector3(const std::string& failoVardas);
+#endif 
