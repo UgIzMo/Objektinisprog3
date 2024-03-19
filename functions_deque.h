00ea54c60e1d88872a5b0cb23d5f4a8d3135013d
UgIzMo <152986822+UgIzMo@users.noreply.github.com>
2024-03-19 13:59:38 +0200

    Update functions_deque.h

--- functions_deque.h
+++ functions_deque.h
@@ -6,6 +6,9 @@
 
 void readDataDeque(std::deque<Studentas>& studentai, const std::string& failoVardas);
 void generateStudentFilesDeque(int size);
-void divideStudentsDeque(const std::string& fileName);
+
+void divideStudentsDeque1(const std::string& fileName);
+void divideStudentsDeque2(const std::string& fileName);
+void divideStudentsDeque3(const std::string& fileName);
 
 #endif 
