0f452aea92b3a0e63bbe8ae3adb6890c7202bcfd
UgIzMo <152986822+UgIzMo@users.noreply.github.com>
2024-03-26 00:04:36 +0200

    Update functions.h

--- functions.h
+++ functions.h
@@ -3,7 +3,7 @@
 
 #include <vector>
 
-enum class ContainerType { None, Vector, List, Deque };
+enum class ContainerType { None, Vector };
 enum class Action { None, Generate, Sort };
 ContainerType getContainerChoice();
 Action getActionChoice();
