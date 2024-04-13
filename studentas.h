6892a00c26e27951a44acc4550ad832ec572210c
UgIzMo <152986822+UgIzMo@users.noreply.github.com>
2024-04-13 23:01:37 +0300

    Update studentas.h

--- studentas.h
+++ studentas.h
@@ -1,42 +1,54 @@
 #ifndef STUDENTAS_H
 #define STUDENTAS_H
 
+#include <iostream>
 #include <string>
 #include <vector>
 
-class Studentas
-{
+class Studentas {
 public:
-    Studentas();                                                      // Constructor
-    Studentas(const std::string &vardas, const std::string &pavarde); // Constructor with parameters
+    // Constructors
+    Studentas();
+    Studentas(const std::string& vardas, const std::string& pavarde);
+    // Destructor
+    ~Studentas();
+    // Copy Constructor
+    Studentas(const Studentas& other);
+    // Copy Assignment Operator
+    Studentas& operator=(const Studentas& other);
+    // Move Constructor
+    Studentas(Studentas&& other) noexcept;
+    // Move Assignment Operator
+    Studentas& operator=(Studentas&& other) noexcept;
 
-    // Public member functions
-    void setVardas(const std::string &vardas);
+    // Getters and Setters
+    void setVardas(const std::string& vardas);
     std::string getVardas() const;
-
-    void setPavarde(const std::string &pavarde);
+    void setPavarde(const std::string& pavarde);
     std::string getPavarde() const;
-
-    void setNamuDarbai(const std::vector<int> &nd);
+    void setNamuDarbai(const std::vector<int>& nd);
     std::vector<int> getNamuDarbai() const;
-
     void setEgzaminas(int egzaminas);
     int getEgzaminas() const;
 
+    // Calculations
     double skaiciuotiVidurki() const;
     double skaiciuotiMediana() const;
     double skaiciuotiGalutini(bool naudotiVidurki) const;
+
+    // Randomization
     void atsitiktiniai();
     void atsitiktiniaiStudentai();
 
-    ~Studentas();
-    
+    // Input/Output Operators
+    friend std::ostream& operator<<(std::ostream& os, const Studentas& student);
+    friend std::istream& operator>>(std::istream& is, Studentas& student);
+
 private:
-    // Private data members
     std::string vardas;
     std::string pavarde;
     std::vector<int> namuDarbai;
     int egzaminas;
 };
 
-#endif // STUDENTAS_H
+#endif
