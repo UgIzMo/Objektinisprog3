2bb757d66c91ffb93cac579b9b6cc7892972e38d
UgIzMo <152986822+UgIzMo@users.noreply.github.com>
2024-04-13 22:58:42 +0300

    Update functions.h

--- functions.h
+++ functions.h
@@ -5,10 +5,10 @@
 
 enum class ContainerType { None, Vector };
 enum class Action { None, Generate, Sort };
-ContainerType getContainerChoice();
+
 Action getActionChoice();
 
 void performAction(ContainerType containerChoice, Action actionChoice, const std::vector<int>& sizes);
 void runApp();
 
-#endif
+#endif // FUNCTIONS_H
