636b3b64d8e9e5a7ce7a0aba8f623c24e4404d4f
UgIzMo <152986822+UgIzMo@users.noreply.github.com>
2024-04-15 21:34:07 +0300

    Update studentas.cpp

--- studentas.cpp
+++ studentas.cpp
@@ -1,121 +1,161 @@
-#include "studentas.h"
+#include <iostream>
+#include <string>
+#include <vector>
 #include <algorithm>
 #include <cstdlib>
-#include <iostream>
 
-// Constructors
-Studentas::Studentas() : egzaminas(0), age(0) {}
+// Abstract Base Class: Person
+class Person {
+public:
+    virtual ~Person() = default;  // Virtual destructor
+    virtual std::string getVardas() const = 0;
+    virtual void setVardas(const std::string& vardas) = 0;
+    virtual std::string getPavarde() const = 0;
+    virtual void setPavarde(const std::string& pavarde) = 0;
+    virtual std::vector<int> getNamuDarbai() const = 0;
+    virtual void setNamuDarbai(const std::vector<int>& nd) = 0;
+    virtual int getEgzaminas() const = 0;
+    virtual void setEgzaminas(int egzaminas) = 0;
 
-Studentas::Studentas(const std::string& vardas, const std::string& pavarde)
-    : vardas(vardas), pavarde(pavarde), egzaminas(0), age(0) {}
+    virtual void addNamuDarbas(int pazymys) = 0;
+    virtual double skaiciuotiVidurki() const = 0;
+    virtual double skaiciuotiMediana() const = 0;
+    virtual double skaiciuotiGalutini(bool naudotiVidurki) const = 0;
+    virtual void atsitiktiniai() = 0;
+    virtual void atsitiktiniaiStudentai() = 0;
+};
 
-// Destructor
-Studentas::~Studentas() {
-    namuDarbai.clear();
-}
+// Derived Class: Student
+class Student : public Person {
+private:
+    std::string vardas;
+    std::string pavarde;
+    std::vector<int> namuDarbai;
+    int egzaminas;
 
-// Getters and Setters
-void Studentas::setVardas(const std::string& vardas) {
-    this->vardas = vardas;
-}
+public:
+    // Constructors
+    Student() : egzaminas(0) {}
+    Student(const std::string& vardas, const std::string& pavarde)
+        : vardas(vardas), pavarde(pavarde), egzaminas(0) {}
 
-std::string Studentas::getVardas() const {
-    return vardas;
-}
-
-void Studentas::setPavarde(const std::string& pavarde) {
-    this->pavarde = pavarde;
-}
-
-std::string Studentas::getPavarde() const {
-    return pavarde;
-}
-
-void Studentas::setNamuDarbai(const std::vector<int>& nd) {
-    namuDarbai = nd;
-}
-
-std::vector<int> Studentas::getNamuDarbai() const {
-    return namuDarbai;
-}
-
-void Studentas::setEgzaminas(int egzaminas) {
-    this->egzaminas = egzaminas;
-}
-
-int Studentas::getEgzaminas() const {
-    return egzaminas;
-}
-
-// Implementing Zmogus virtual functions
-int Studentas::getAge() const {
-    // Implement logic to calculate and return the age of the student
-    // For example, if 'age' is stored as a member variable or computed based on birthdate:
-    return age;
-}
-
-std::string Studentas::getName() const {
-    // Implement logic to construct and return the full name of the student
-    return vardas + " " + pavarde;
-}
-
-// Calculations
-double Studentas::skaiciuotiVidurki() const {
-    double suma = 0.0;
-    for (int pazymys : namuDarbai) {
-        suma += pazymys;
+    // Destructor
+    ~Student() override {
+        namuDarbai.clear();
+        vardas.clear();
+        pavarde.clear();
+        egzaminas = 0;
     }
-    return namuDarbai.empty() ? 0.0 : suma / namuDarbai.size();
-}
 
-double Studentas::skaiciuotiMediana() const {
-    if (namuDarbai.empty()) return 0.0;
-    std::vector<int> tempNamuDarbai = namuDarbai;
-    std::sort(tempNamuDarbai.begin(), tempNamuDarbai.end());
-    int dydis = tempNamuDarbai.size();
-    return (dydis % 2 == 0) ? (tempNamuDarbai[dydis / 2 - 1] + tempNamuDarbai[dydis / 2]) / 2.0 : tempNamuDarbai[dydis / 2];
-}
+    // Copy Constructor
+    Student(const Student& other)
+        : vardas(other.vardas), pavarde(other.pavarde),
+          namuDarbai(other.namuDarbai), egzaminas(other.egzaminas) {}
 
-double Studentas::skaiciuotiGalutini(bool naudotiVidurki) const {
-    double galutinis = naudotiVidurki ? (0.4 * skaiciuotiVidurki() + 0.6 * egzaminas) : (0.4 * skaiciuotiMediana() + 0.6 * egzaminas);
-    return galutinis;
-}
-
-// Randomization
-void Studentas::atsitiktiniai() {
-    namuDarbai.resize(rand() % 10 + 1);
-    for (int& pazymys : namuDarbai) {
-        pazymys = rand() % 10 + 1;
+    // Assignment Operator
+    Student& operator=(const Student& other) {
+        if (this != &other) {
+            vardas = other.vardas;
+            pavarde = other.pavarde;
+            namuDarbai = other.namuDarbai;
+            egzaminas = other.egzaminas;
+        }
+        return *this;
     }
-    egzaminas = rand() % 10 + 1;
-}
 
-void Studentas::atsitiktiniaiStudentai() {
-    const char* vardai[] = {"Ana", "Gloria", "Sofia", "Ugne", "Aina", "Guoda"};
-    const char* pavardes[] = {"Mockute", "Liuc", "Mickute", "Macaite", "Migonyte", "Peleda"};
-    int vardasIndex = rand() % 6;
-    int pavardeIndex = rand() % 6;
-    vardas = vardai[vardasIndex];
-    pavarde = pavardes[pavardeIndex];
-    atsitiktiniai();
-}
-
-// Output Operator (Serialization)
-std::ostream& operator<<(std::ostream& os, const Studentas& student) {
-    os << student.vardas << " " << student.pavarde << " " << student.egzaminas << " ";
-    for (int pazymys : student.namuDarbai) {
-        os << pazymys << " ";
+    // Move Constructor
+    Student(Student&& other) noexcept
+        : vardas(std::move(other.vardas)), pavarde(std::move(other.pavarde)),
+          namuDarbai(std::move(other.namuDarbai)), egzaminas(other.egzaminas) {
+        other.egzaminas = 0;
     }
-    return os;
-}
 
-// Input Operator (Deserialization)
-std::istream& operator>>(std::istream& is, Studentas& student) {
-    is >> student.vardas >> student.pavarde >> student.egzaminas;
-    student.namuDarbai.clear();
-    int pazymys;
-    while (is >> pazymys) {
-        student.namuDarbai.push_back(pazymys);
+    // Move Assignment Operator
+    Student& operator=(Student&& other) noexcept {
+        if (this != &other) {
+            vardas = std::move(other.vardas);
+            pavarde = std::move(other.pavarde);
+            namuDarbai = std::move(other.namuDarbai);
+            egzaminas = other.egzaminas;
+            other.egzaminas = 0;
+        }
+        return *this;
     }
-    return is;
-}
+
+    // Getter and Setter Methods
+    std::string getVardas() const override { return vardas; }
+    void setVardas(const std::string& vardas) override { this->vardas = vardas; }
+
+    std::string getPavarde() const override { return pavarde; }
+    void setPavarde(const std::string& pavarde) override { this->pavarde = pavarde; }
+
+    std::vector<int> getNamuDarbai() const override { return namuDarbai; }
+    void setNamuDarbai(const std::vector<int>& nd) override { namuDarbai = nd; }
+
+    int getEgzaminas() const override { return egzaminas; }
+    void setEgzaminas(int egzaminas) override { this->egzaminas = egzaminas; }
+
+    void addNamuDarbas(int pazymys) override { namuDarbai.push_back(pazymys); }
+
+    // Calculation Methods
+    double skaiciuotiVidurki() const override {
+        double suma = 0.0;
+        for (int pazymys : namuDarbai) {
+            suma += pazymys;
+        }
+        return namuDarbai.empty() ? 0.0 : suma / namuDarbai.size();
+    }
+
+    double skaiciuotiMediana() const override {
+        if (namuDarbai.empty()) return 0.0;
+        std::vector<int> tempNamuDarbai = namuDarbai;
+        std::sort(tempNamuDarbai.begin(), tempNamuDarbai.end());
+        int dydis = tempNamuDarbai.size();
+        return (dydis % 2 == 0) ? (tempNamuDarbai[dydis / 2 - 1] + tempNamuDarbai[dydis / 2]) / 2.0
+                                : tempNamuDarbai[dydis / 2];
+    }
+
+    double skaiciuotiGalutini(bool naudotiVidurki) const override {
+        double galutinis = naudotiVidurki ? (0.4 * skaiciuotiVidurki() + 0.6 * egzaminas)
+                                            : (0.4 * skaiciuotiMediana() + 0.6 * egzaminas);
+        return galutinis;
+    }
+
+    void atsitiktiniai() override {
+        namuDarbai.resize(rand() % 10 + 1);
+        for (int& pazymys : namuDarbai) {
+            pazymys = rand() % 10 + 1;
+        }
+        egzaminas = rand() % 10 + 1;
+    }
+
+    void atsitiktiniaiStudentai() override {
+        const char* vardai[] = {"Morta", "Aina", "Guoda", "Marija", "Paulina"};
+        const char* pavardes[] = {"Petraityte", "Jurksaityte", "Mockute", "Macaite", "Liekyte"};
+        int vardasIndex = rand() % 5; // 5, nes masyvas prasideda nuo 0
+        int pavardeIndex = rand() % 5; // 5, nes masyvas prasideda nuo 0
+        vardas = vardai[vardasIndex];
+        pavarde = pavardes[pavardeIndex];
+        atsitiktiniai();
+    }
+
+    // Output Operator (Serialization)
+    friend std::ostream& operator<<(std::ostream& os, const Student& student) {
+        os << student.vardas << " " << student.pavarde << " " << student.egzaminas << " ";
+        for (int pazymys : student.namuDarbai) {
+            os << pazymys << " ";
+        }
+        return os;
+    }
+
+    // Input Operator (Deserialization)
+    friend std::istream& operator>>(std::istream& is, Student& student) {
+        is >> student.vardas >> student.pavarde >> student.egzaminas;
+        student.namuDarbai.clear();
+        int pazymys;
+        while (is >> pazymys) {
+            student.namuDarbai.push_back(pazymys);
+        }
+        return is;
+    }
+};
