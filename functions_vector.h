093fd2c2a67535d5f182c98123d158fbdc126361
UgIzMo <152986822+UgIzMo@users.noreply.github.com>
2024-04-29 11:04:07 +0300

    Update functions_vector.h

--- functions_vector.h
+++ functions_vector.h
@@ -5,10 +5,30 @@
 #include <string>
 #include <vector>
 
-// Function declarations
+/**
+ * @brief Funkcijos deklaracijos.
+ */
+
+/**
+ * @brief Įveda duomenis apie studentus iš failo į vektorių.
+ * 
+ * @param studentai Vektorius, kuriame bus saugomi studentų duomenys.
+ * @param failoVardas Failo pavadinimas, iš kurio bus skaityti duomenys.
+ */
 void readDataVector(std::vector<Studentas>& studentai, const std::string& failoVardas);
+
+/**
+ * @brief Generuoja studentų failus pagal nurodytą dydį ir atsitiktinius duomenis.
+ * 
+ * @param size Studentų failų dydis.
+ */
 void generateStudentFilesVector(int size);
 
+/**
+ * @brief Rusiuoja studentus ir išveda juos į naują failą (versija 1).
+ * 
+ * @param failoVardas Naujo failo pavadinimas, į kurį bus išsaugoti surūšiuoti studentai.
+ */
 void rusiuotStudentusVector1(const std::string& failoVardas);
 
 #endif // FUNCTIONS_VECTOR_H
