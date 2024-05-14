f3a46509dc92f881cbd3495328e992d77f543746
UgIzMo <152986822+UgIzMo@users.noreply.github.com>
2024-05-14 15:00:24 +0300

    Delete html directory

--- html/search/all_6.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['getegzaminas_0',['getEgzaminas',['../class_studentas.html#a00394656352d05b8061f7caf650861e4',1,'Studentas::getEgzaminas()'],['../class_human.html#ab1f823bacbd18d51d4a0781fde44a8d8',1,'Human::getEgzaminas()']]],
-  ['getnamudarbai_1',['getNamuDarbai',['../class_studentas.html#ae48ce8e212f4e4c2862c29937d20611e',1,'Studentas::getNamuDarbai()'],['../class_human.html#a1544911e1b02e476a730640b4394312c',1,'Human::getNamuDarbai()']]],
-  ['getpavarde_2',['getPavarde',['../class_studentas.html#ac525bcbd0e6ed2cd346e4152b7e1ba3e',1,'Studentas::getPavarde()'],['../class_human.html#aa4dd89c941592bc83868681ce08c2fc1',1,'Human::getPavarde()']]],
-  ['getvardas_3',['getVardas',['../class_studentas.html#afe3b201c91fadef0e6a57876da303f38',1,'Studentas::getVardas()'],['../class_human.html#a2a210725f1b9d696ce74899aff9ba2db',1,'Human::getVardas()']]]
-];
--- html/search/all_d.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['testruleoffive_0',['testRuleOfFive',['../class_studentas.html#a25844f1d80314a8722593f0b49b34de0',1,'Studentas']]]
-];
