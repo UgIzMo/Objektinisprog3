8df6d1eee1e44ebe3dd24a2c8e01506f97177bd9
UgIzMo <152986822+UgIzMo@users.noreply.github.com>
2024-05-14 15:07:42 +0300

    Delete html directory

--- html/search/functions_0.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['addnamudarbas_0',['addNamuDarbas',['../class_studentas.html#a9bc52867422c706c2c9592bcf1e2b04f',1,'Studentas::addNamuDarbas()'],['../class_human.html#a69a32ea4d2ff5c441764149d85f9a166',1,'Human::addNamuDarbas()']]],
-  ['atsitiktiniai_1',['atsitiktiniai',['../class_studentas.html#ae8efa77bb84ab9fb853589720f3faef8',1,'Studentas::atsitiktiniai()'],['../class_human.html#a914b13a68259668e463a84c244eb547c',1,'Human::atsitiktiniai()']]],
-  ['atsitiktiniaistudentai_2',['atsitiktiniaiStudentai',['../class_studentas.html#af80ed9d54c1e8b360e8ec321e746f8bc',1,'Studentas::atsitiktiniaiStudentai()'],['../class_human.html#afdd31aadd8833f79849f8020299fd48f',1,'Human::atsitiktiniaiStudentai()']]]
-];
